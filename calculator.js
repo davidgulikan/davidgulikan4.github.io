b1ff1e5b8527e0f969738666e36943afe0ea4465
davidgulikan <gulikand99@gmail.com>
2024-10-27 15:26:48 +0300

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,6 +1,15 @@
-function calculateTotal() {
-    const quantity = document.getElementById("quantity").value;
-    const price = document.getElementById("product").value;
-    const total = quantity * price;
-    document.getElementById("result").textContent = `Общая стоимость заказа: ${total}₽`;
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const quantityInput = document.getElementById("quantity");
+    const productSelect = document.getElementById("product");
+    const calculateBtn = document.getElementById("calculateBtn");
+    const resultOutput = document.getElementById("result");
+
+    function calculateTotal() {
+        const quantity = quantityInput.value;
+        const price = productSelect.value;
+        const total = quantity * price;
+        resultOutput.textContent = `Общая стоимость заказа: ${total}₽`;
+    }
+
+    calculateBtn.addEventListener("click", calculateTotal);
+});
